453a76e94066bc8e0e9ce86d5001f3794c1b2f09
Asad Khan <asad60khan@gmail.com>
2018-09-05 17:25:17 +0200

    footer correction

--- web/js/accueil.js
+++ web/js/accueil.js
@@ -10,6 +10,12 @@ $(document).ready(function () {
     });
 
 
+    var last_section = $(".section:last");
+    var padding_bottom = parseInt($(last_section).css("padding-bottom"));
+    padding_bottom += parseInt(padding_bottom);
+    $(last_section).css("padding-bottom", padding_bottom + "px");
+    
+
     $(".btn-devis").click(function (ev) {
         ev.preventDefault();
         $('html, body').animate({
