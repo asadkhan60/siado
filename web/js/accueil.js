5de1bb907140c74a3b5c48f5c67aaab9297811fc
Asad Khan <asad60khan@gmail.com>
2018-09-05 19:00:50 +0200

    menu home added and fonctionnement section

--- web/js/accueil.js
+++ web/js/accueil.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
     var padding_bottom = parseInt($(last_section).css("padding-bottom"));
     padding_bottom += parseInt(padding_bottom);
     $(last_section).css("padding-bottom", padding_bottom + "px");
-    
+
 
     $(".btn-devis").click(function (ev) {
         ev.preventDefault();
